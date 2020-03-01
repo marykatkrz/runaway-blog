710ba05967f252e3f5bd2b1b1140a826db7f710e
Mary Krzesicki <krzesicki.mary@gmail.com>
2020-03-01 15:58:30 -0800

    form work

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,6 +1,6 @@
 <template>
   <Layout>
-    <form id="contact" name="contact" method="POST" data-netlify="true">
+    <form id="contact" name="contact" action="/pages/success" method="POST" data-netlify="true">
       <h2>Contact Form</h2>
       <em>Questions? Comments? Inquiries? Feel free to use this form or send emails to runawaymaryk@gmail.com and I will get back to you as soon as possible!</em>
       <p>
---
+++ src/pages/Success.vue
@@ -0,0 +1,14 @@
+<template>
+  <Layout>
+    <h2>Thank you!</h2>
+    <p>Your form has sent.</p>
+    <div id="return">
+      <a href="/">Back to Home</a>
+    </div>
+  </Layout>
+</template>
+<style scoped>
+    h2{
+        margin: 100px;
+    }
+</style>
