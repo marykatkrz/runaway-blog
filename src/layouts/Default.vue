ed546a42b49a371b72dc69948942152456880afb
Mary Krzesicki <krzesicki.mary@gmail.com>
2021-01-14 20:28:05 -0800

    more testing

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -8,7 +8,7 @@
         <a href="/">Home</a>
         <a href="/about/">About</a>
         <a href="/blog/">Blog</a>
-        <!-- <a href="/contact/">Contact</a> -->
+        <a href="/contact/">Contact</a>
       </div>
       </div>     
       <a href="/">
