c32379e021d0ddf78c4f94043ad202271b135f70
Mary Krzesicki <krzesicki.mary@gmail.com>
2021-01-17 20:19:31 -0800

    contact form change

--- src/pages/Success.vue
+++ src/pages/Success.vue
@@ -4,6 +4,7 @@
     <p>Your form has submitted.</p>
     <div id="return">
       <a href="/">Back to Home</a>
+      <g-image id="plane" src="~/assets/plane.jpg"></g-image>
     </div>
   </Layout>
 </template>
@@ -11,4 +12,7 @@
     h2{
         margin: 100px;
     }
+    #plane{
+        width: 80%;
+}
 </style>
